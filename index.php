05031d5b96f145d1867d809b3d5d9f3c72863ae0
Geovani carlos de souza <94084031+Geovani237@users.noreply.github.com>
2022-04-14 22:20:25 -0300

    Criação incompleta da 1 pagina

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SGLachonete 1.0</title>
+    <link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+    <h1>SGLanchonete 1.0</h1>
+    <a href ="#" class ="botao1">Cadastro de Produtos</a>
+    <a href = "#" class ="botao2">Cadastro de pedidos </a>
+    
+
+</body>
+</html>
